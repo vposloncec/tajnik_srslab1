69914c98148e18be8ec413c7e97441f7b3ae6639
Viktor Posloncec <viktor.posloncec@gmail.com>
2021-03-19 12:00:55 +0100

    Storage: Add module function LoadDecrypt
    Signed-off-by: Viktor Posloncec <viktor.posloncec@gmail.com>

--- persistance/storage.go
+++ persistance/storage.go
@@ -2,6 +2,7 @@ package persistance
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/vposloncec/lab1-srs/cripter"
 	"io"
 	"io/ioutil"
@@ -9,12 +10,24 @@ import (
 
 type Storage map[string]string
 
+func LoadDecrypt(passphrase string, r io.Reader) (Storage, error){
+	s := Storage{}
+	if err := s.LoadDecrypt(passphrase, r); err != nil{
+		return nil, err
+	}
+	return s, nil
+}
+
 func (s *Storage) LoadDecrypt(passphrase string, r io.Reader) error {
 	bCipher, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
 	}
 	b, err := cripter.Decrypt(bCipher, passphrase)
+	if err != nil {
+		return fmt.Errorf("Master file could not be decrypted, perhaps the master password was wrong?")
+	}
+	// fmt.Println("Decrypted! ", string(b))
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
@@ -26,6 +39,7 @@ func (s *Storage) SaveEncrypt(passphrase string, w io.Writer) error {
 	if err != nil {
 		return err
 	}
+	// fmt.Println("saving: ", string(b))
 	bCipher, err := cripter.Encrypt(b, passphrase)
 	if err != nil {
 		return err
